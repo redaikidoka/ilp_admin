fb4a81b837d14740fc57d828fa7d9d7b0041b0c9
Steven T Ancheta <steventancheta@gmail.com>
2016-08-25 09:11:08 -0700

    SASS works

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'home',
-  styleUrls: ['./home.css'],
+  styleUrls: ['./home.scss'],
   templateUrl: './home.pug'
 })
 export class Home {
 }
-
-
